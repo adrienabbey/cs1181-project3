ca31bb76cddf5699da4b8a0de1d050f3b2b7fd12
Adrien Abbey <abbey.12@wright.edu>
2022-03-24 06:17:55 -0400

    Initial basic implementation of Checkout

---
+++ Checkout.java
@@ -0,0 +1,15 @@
+// Adrien Abbey, CS-1181L-07, Mar. 24, 2022
+// Checkout class for Project 3
+
+// Checkout lanes are Queues (LinkedLists) for Customers.
+// Checkout lanes come in two types: regular and express.
+
+import java.util.LinkedList;
+
+public class Checkout extends LinkedList<Customer> {
+
+    public Checkout() {
+        super();
+    }
+
+}
